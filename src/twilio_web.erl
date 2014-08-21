447d39ce4a64f613a324fc4d44428c0fac74df4e
Shane Logsdon <shane@shanelogsdon.com>
2014-08-21 12:02:41 -0400

    ensure our environment variable is interpreted as a string

--- src/twilio_web.erl
+++ src/twilio_web.erl
@@ -11,7 +11,7 @@
 
 -include("twilio.hrl").
 
--define(DEFAULT_PORT, os:getenv("TWILIO_PORT")).
+-define(DEFAULT_PORT, string:to_integer(os:getenv("TWILIO_PORT"))).
 
 %% @equiv start(8080)
 start() ->
